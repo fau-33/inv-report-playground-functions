6e5496eb9610af3ccd7ab801ad0ae55f151aa970
Flávio Félix <fau_felix30@hotmail.com>
2022-12-06 16:07:13 -0300

    Feito o desafio 9 enconde

--- src/challenges.js
+++ src/challenges.js
@@ -71,6 +71,24 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9 - Crie a função encode e a função decode
+function encode(string) {
+  let novaString = string;
+  novaString = novaString.replace(/a/g, '1');
+  novaString = novaString.replace(/e/g, '2');
+  novaString = novaString.replace(/i/g, '3');
+  novaString = novaString.replace(/o/g, '4');
+  novaString = novaString.replace(/u/g, '5');
+  return novaString;
+}
+function decode(string) {
+  let novaString = string;
+  novaString = novaString.replace(/1/g, 'a');
+  novaString = novaString.replace(/2/g, 'e');
+  novaString = novaString.replace(/3/g, 'i');
+  novaString = novaString.replace(/4/g, 'o');
+  novaString = novaString.replace(/5/g, 'u');
+  return novaString;
+}
 
 // Desafio 10 - Crie a função techList
 
