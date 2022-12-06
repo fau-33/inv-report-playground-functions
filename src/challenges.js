8f94906fda4a06f5fdf54f075a5aea70cdccb0cf
Flávio Félix <fau_felix30@hotmail.com>
2022-12-06 15:53:07 -0300

    Feito o desafio 5 highestCoun

--- src/challenges.js
+++ src/challenges.js
@@ -25,6 +25,21 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 5 - Crie a função highestCount
+function highestCount(array) {
+  let maior = array[0];
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] > maior) {
+      maior = array[index];
+    }
+  }
+  let contador = 0;
+  for (let secondIndex = 0; secondIndex < array.length; secondIndex += 1) {
+    if (maior === array[secondIndex]) {
+      contador += 1;
+    }
+  }
+  return contador;
+}
 
 // Desafio 6 - Crie as funções calcTriangleArea, calcRectangleArea e calcAllAreas
 
